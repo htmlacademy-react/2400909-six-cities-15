19cc946c459b7f2fa90e35ae519affe6eb3a7c4f
Дмитрий Остапенко <nugashop@yandex.ru>
2024-02-25 19:28:39 +0600

    Update link Logo & Email

--- src/pages/favorites-empty-page/favorites-empty-page.tsx
+++ src/pages/favorites-empty-page/favorites-empty-page.tsx
@@ -1,3 +1,6 @@
+import Logo from '../../components/logo';
+import Email from '../../components/email';
+
 function FavoritesEmptyPage(): JSX.Element {
   return (
     <div className="page page--favorites-empty">
@@ -5,19 +8,12 @@ function FavoritesEmptyPage(): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
+              <Logo />
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">0</span>
-                  </a>
+                  <Email />
                 </li>
                 <li className="header__nav-item">
                   <a className="header__nav-link" href="#">
