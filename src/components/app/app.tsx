b02e13e18ca9aa8d37352410f854f6f04ec64284
Дмитрий Остапенко <nugashop@yandex.ru>
2024-02-25 16:44:14 +0600

    Добавляем маршрутизацию

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import { Route, BrowserRouter, Routes, Link } from 'react-router-dom';
+import AppRoute from '../const';
 import MainPage from '../../pages/main-page';
+import FavoritesEmptyPage from '../../pages/favorites-empty-page';
+import FavoritesPage from '../../pages/favorites-page';
+import LoginPage from '../../pages/login-page';
+import MainEmptyPage from '../../pages/main-empty-page';
+import OfferPage from '../../pages/offer-page';
+import OfferNotLoggedPage from '../../pages/offer-not-logged-page';
+import { Fragment } from 'react';
 
 type AppProps = {
   placeCount: number;
@@ -6,7 +15,39 @@ type AppProps = {
 
 function App({placeCount}: AppProps): JSX.Element {
   return (
-    <MainPage placeCount = {placeCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage placeCount = {placeCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesPage />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path="*"
+          element={
+            <Fragment>
+              <h1>
+                404.
+                <br />
+                <small>Page is not found</small>
+              </h1>
+              <Link to="/">Go to main page</Link>
+            </Fragment>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
