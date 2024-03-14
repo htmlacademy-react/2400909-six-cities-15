21d24ba5c3d57f14f37963462d2745a2d6a915e3
Дмитрий Остапенко <nugashop@yandex.ru>
2024-03-14 14:16:36 +0600

    fix: app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -11,20 +11,20 @@ import NotFoundPage from '../not-found-page';
 import Layout from '../layout/layout';
 import { getAuthorizationStatus } from '../../authorizationStatus';
 import { Offer } from '../../types/offer';
-import { CityName } from '../../types/city-name';
 import { Comment } from '../../types/comment';
-import { ExtendedOffer } from '../../types/extended-offer';
+import { City } from '../../types/offer';
+import { CityName } from '../../types/city-name';
 
 type AppProps = {
   placeCount: number;
   offers: Offer[];
   favorites: Offer[];
-  cities: CityName[];
-  extendedOffers: ExtendedOffer[];
+  cities: City[];
+  citiesName: CityName[];
   comments: Comment[];
 }
 
-function App({placeCount, offers, favorites, cities, extendedOffers, comments}: AppProps): JSX.Element {
+function App({placeCount, offers, favorites, cities, citiesName, comments}: AppProps): JSX.Element {
   const authorizationStatus = getAuthorizationStatus();
 
   return (
@@ -42,7 +42,7 @@ function App({placeCount, offers, favorites, cities, extendedOffers, comments}: 
                 <MainPage
                   placeCount = {placeCount}
                   offers = {offers}
-                  cities = {cities}
+                  citiesName = {citiesName}
                 />
               }
             />
