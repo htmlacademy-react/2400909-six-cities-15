ddae6813c4681a9f193f89d51ffb431d79112236
Дмитрий Остапенко <nugashop@yandex.ru>
2024-04-01 14:49:55 +0600

    Добавит обработку ошибок взаимодействия с сервером

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,15 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from "axios";
+import axios, {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} from "axios";
 import { getToken } from "./token";
+import { StatusCodes } from "http-status-codes";
+import { processErrorHandle } from "./process-error-handle";
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -21,6 +31,17 @@ export const createAPI = (): AxiosInstance => {
       return config;
     },
   );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<{error: string}>) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        processErrorHandle(error.response.data.error);
+      }
+
+      throw error;
+    }
+  )
 
   return api;
 };
