16a383077f7f61a2689c3ce6e8d4a04cc1d0e23c
Дмитрий Остапенко <nugashop@yandex.ru>
2024-04-10 15:22:23 +0600

    fix: favorites-page

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -12,17 +12,26 @@ function FavoritesPage(): JSX.Element {
 
   return (
     <div className="page">
-      <main className="page__main page__main--favorites">
+      <main className={`page__main page__main--favorites${favorites.length > 0 ? '' : ' page__main--favorites-empty'}`}>
         <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {
-                uniqueCities.map((city) =>
-                  (<FavoritesCities favorites={favorites} city={city as CityName} key={city}/>))
-              }
-            </ul>
-          </section>
+          {favorites.length > 0 ? (
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                {
+                  uniqueCities.map((city) =>
+                    (<FavoritesCities favorites={favorites} city={city as CityName} key={city}/>))
+                }
+              </ul>
+            </section>) : (
+              <section className="favorites favorites--empty">
+                <h1 className="visually-hidden">Favorites (empty)</h1>
+                <div className="favorites__status-wrapper">
+                  <b className="favorites__status">Nothing yet saved.</b>
+                  <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+                </div>
+              </section>
+            )}
         </div>
       </main>
     </div>
